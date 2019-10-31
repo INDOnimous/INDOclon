4b5815191080b7971d3294f911274061cba722e3
INDOnimous <40731452+INDOnimous@users.noreply.github.com>
2019-10-31 08:02:26 +0700

    Update INDOdef.py

--- INDOdef/INDOdef.py
+++ INDOdef/INDOdef.py
@@ -1,1 +1,20 @@
-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#####DONT CHANGE THIS########
+######################
+### Script By INDOnimous
+### www.pasaronlen.com
+### Don't Change This.!!!
+######################
+import os
+import sys
+os.system("clear")
+os.system("mkdir INDOnimous")
+os.system("mv INDOnimous/ /storage/emulated/0/")
+os.system("mv INDOscript.html /storage/emulated/0/INDOnimous/")
+print
+print("Data Sedang Di pindahkan, Silahkan Tunggu.")
+os.system("sleep 5")
+print("Berhasil Di pindahkan ke /storage/emulated/0/INDOnimous/INDOscript.html")
+os.system("sleep3")
+print("[ Script Siap Di gunakan ]")
